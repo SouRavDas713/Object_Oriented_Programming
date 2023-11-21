a20c4ad92eebb5df5da5c7436de0c9effcd5f6d8
SouRavDas713 <sdsourav713@gmail.com>
2023-11-21 19:36:56 +0600

    Create Multiple_Inheritence.cpp

---
+++ Inheritence/Multiple_Inheritence.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class a
+{
+    public:
+    void printa()
+    {
+        cout<<"I am From A class"<<endl;
+    }
+};
+class b
+{
+    public:
+    void printb()
+    {
+        cout<<"I am From B class"<<endl;
+    }
+};
+class c
+{
+    public:
+    void printc()
+    {
+        cout<<"I am From C class"<<endl;
+    }
+};
+//Most Importent Part
+class d : public a,public b,public c
+{
+    public:
+    void printd()
+    {
+        cout<<"I am From D class"<<endl;
+    }
+};
+int main()
+{
+    d ob;
+    ob.printa();
+    ob.printb();
+    ob.printc();
+    ob.printd();
+    return 0;
+} 
